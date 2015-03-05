30eed92924ec4289804c717c8c770b571a28c945
Ayla Dantas <ayladebora@gmail.com>
2015-03-05 12:45:05 -0300

    Acrescentando parâmetros no criarAluno e no cadastrarRespostaQuestaoMEPorId

--- src/br/ufpb/educservice/AlunoDAO.java
+++ src/br/ufpb/educservice/AlunoDAO.java
@@ -3,10 +3,10 @@ package br.ufpb.educservice;
 import java.util.List;
 
 public interface AlunoDAO {
-	public Aluno criarAluno();
+	public Aluno criarAluno(String login, String senha);
 	public void cadastraRespostaVFDeAluno(String idAluno, String idExercicio, String idQuestao,  List<String> respostas);
-	public void cadastrarRespostaQuestaoMEPorId(String idAluno, String idExercicio, int alternativaCorreta);
+	public void cadastrarRespostaQuestaoMEPorId(String idAluno, String idExercicio, String idQuestao, int alternativaCorreta);
 	public void cadastrarRespostaQuestaoDissertativaPorId(String idAluno, String idExercicio, String idQuestao,  String solucao);
-	public void removerAluno();
+	public void removerAluno(String idAluno);
 	public Questao consultaRespostaDeAluno(String idAluno, String idExercicio, String idQuestao);
 }
